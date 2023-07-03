ddc8788dfda5c996c82d3de674017d9fc033f64a
Caroline de Oliveira <130517427+OliveiraKroll@users.noreply.github.com>
2023-07-03 11:23:35 -0300

    Update at6.js

--- at6.js
+++ at6.js
@@ -9,3 +9,22 @@ const rl = readline.createInterface({
     output: process.stdout
 
 });
+
+
+rl.question("Digite a lista de números, separando por vírgula: ", (listaNumeros) => {
+
+const numeros = listaNumeros.split(',').map(num => parseFloat(num.trim()));
+const numerosPares = numeros.filter(num => num % 2 === 0);
+
+if(numerosPares.length > 0){
+    const somaPares = numerosPares.reduce((acc, num) => acc + num);
+    const mediaPares = somaPares / numerosPares.length;
+    console.log("O resultadio da média dos pares é: ", mediaPares);
+} else {
+    console.log("Não há númeos pares ou os valores são inválidos");
+}
+
+rl.close();
+
+
+});
